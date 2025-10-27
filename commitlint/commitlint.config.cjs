7745a470553102fff6bcd7546392e794eb4ec9ab
Kelvin Calcano <kelvinr02@hotmail.com>
2025-10-27 10:00:17 -0400

    chore: add commitlint estructure for commits

---
+++ commitlint/commitlint.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert'
+      ]
+    ],
+    'scope-case': [2, 'always', ['kebab-case', 'lower-case']],
+    'header-max-length': [2, 'always', 72],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+  },
+};
