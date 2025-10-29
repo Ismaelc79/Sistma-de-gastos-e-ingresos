dd518bb6d28d6f55a1cd02414f4b9ae90e8a6d10
Briangel Ismael Abreu Hernandez <briangel.abreu@inabima.gob.do>
2025-10-29 11:08:35 -0400

    feat(api): add infrastructure using dapper #8

---
+++ backend-dotnet/src/Infrastructure/Persistence/Context/DapperContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence.Context
+{
+    internal class DapperContext
+    {
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration) 
+        {
+            _connectionString = configuration.GetConnectionString("GastosIngresosDb")!;
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
