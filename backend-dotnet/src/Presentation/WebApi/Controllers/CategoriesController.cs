ad5953018397ffd028145fc750c10ba2347fd5cf
Alberto Nuñez <jalbertonunezf@gmail.com>
2025-10-31 16:18:31 -0400

    Mis cambios

--- backend-dotnet/src/Presentation/WebApi/Controllers/CategoriesController.cs
+++ backend-dotnet/src/Presentation/WebApi/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Net;
+using Application.DTOs.Categories;
+using Application.DTOs.Transactions;
+using Application.Services;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -9,21 +12,38 @@ namespace WebApi.Controllers
 
     [ApiController]
     [Route("/api/[controller]")]
+
+
      public class CategoriesController: ControllerBase
+
     {
 
-            
+        readonly CategoryService categoryService;
+
+
         [HttpGet]
         public IActionResult getCategories(){
 
-            return Ok();
+            return Ok(categoryService.GetCategoriesByUserIdAsync);
+           
         }
 
         [HttpPost]
-        public IActionResult addCategorie()
+        public IActionResult addCategorie(CreateCategoryRequest createCategory)
         {
 
-            return Created("","");
+            try
+            {
+
+              Task<CategoryDto> categoria = categoryService.CreateCategoryAsync(createCategory);
+                return Created("", categoria);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex );
+            }
+
+
         }
 
     }
